22b3078273542d7983ee7daca83f2438ac4d2ee9
Steven <30360094+stevenpeniche@users.noreply.github.com>
2017-09-28 14:53:21 -0400

    Checkpoint 5 - Controllers: complete

---
+++ app/scripts/fixtures.js
@@ -0,0 +1,14 @@
+var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26', soundFileUrl: 'assets/music/blue.mp3' },
+         { title: 'Green', duration: '3:14', soundFileUrl: 'assets/music/green.mp3' },
+         { title: 'Red', duration: '5:01', soundFileUrl: 'assets/music/red.mp3' },
+         { title: 'Pink', duration: '3:21', soundFileUrl: 'assets/music/pink.mp3' },
+         { title: 'Magenta', duration: '2:15', soundFileUrl: 'assets/music/magenta.mp3' }
+     ]
+ };
